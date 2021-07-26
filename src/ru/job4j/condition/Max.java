e4ec2d8a0708ad2f3bbcb8ad818f554a09a13bdf
MrRocketR <kuptovns@gmail.com>
2021-07-26 19:46:01 +0300

    4.2. Тернарное сравнение [#189 #181578]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,11 +2,11 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
+        int result = left >= right ? left : right;
         return result;
     }
 
     public static void main(String[] args) {
-        System.out.println(max(2, 3));
+        System.out.println(max(3, 3));
     }
 }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -8,11 +8,29 @@ import static org.junit.Assert.*;
 public class MaxTest {
 
     @Test
-    public void max() {
+    public void whenRightMax() {
         int l = 1;
         int r = 2;
         int expected = 2;
         int result = Max.max(1, 2);
+        Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void whenLeftmax() {
+        int l = 2;
+        int r = 1;
+        int expected = 2;
+        int result = Max.max(2, 1);
+        Assert.assertEquals(2, result);
+    }
+
+    @Test
+    public void whenAllEquals() {
+        int l = 2;
+        int r = 2;
+        int expected = 2;
+        int result = Max.max(2, 2);
         Assert.assertEquals(2, result);
     }
 }
