6a7774028cf535a4755f52b4a1afff29f0cf5932
MrRocketR <kuptovns@gmail.com>
2021-08-02 21:33:56 +0300

    6.0.4. Заполнить массив степенями чисел. [#33488]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,18 +2,18 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short [] ages = new short [10];
+        short[] ages = new short[10];
         System.out.println("Размер массива равен: " + ages.length);
-        String [] surnames = new String [100500];
+        String[] surnames = new String[100500];
         System.out.println("Размер массива равен: " + surnames.length);
-        float [] prices = new float [40];
+        float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
-        String [] names = new String[4];
+        String[] names = new String[4];
         names[0] = "Nikita";
         names[1] = "Andrew";
         names[2] = "Valera";
         names[3] = "Masha";
-        for (String namesOut: names) {
+        for (String namesOut : names) {
             System.out.println(namesOut);
         }
     }
